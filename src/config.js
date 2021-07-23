d05c969bb0540e1ddf9ef23359a224867fb8d614
ky0yk <ksaik1024@gmail.com>
2021-07-23 16:54:32 +0900

    Configure API invokeURL

--- src/config.js
+++ src/config.js
@@ -2,6 +2,6 @@
 
 module.exports = {
     api: {
-        invokeUrl: '' // e.g. https://rc2345678345.execute-api.us-west-2.amazonaws.com/prod,
+        invokeUrl: ' https://xur200grs8.execute-api.ap-northeast-1.amazonaws.com/prod' // e.g. https://rc2345678345.execute-api.us-west-2.amazonaws.com/prod,
     }
 }
